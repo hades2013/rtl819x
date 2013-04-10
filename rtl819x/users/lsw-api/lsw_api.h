a09d43c6e9c1a1228caab49079edcfa18ad814cc
einsn@vmubuntu-pc <179712066@qq.com>
2013-04-10 11:58:08 +0800

    do first simple test for lsw-api, everything runs well

--- rtl819x/users/lsw-api/lsw_api.h
+++ rtl819x/users/lsw-api/lsw_api.h
@@ -1,3 +1,9 @@
+#ifndef __LSW_API_H__
+#define __LSW_API_H__
+
+#include <stdint.h>
+
+int rtl8198_set_vlan(uint32_t vlan, uint32_t port_mask, uint32_t untag_mask, uint32_t fid);
 
 
-
+#endif /* __LSW_API_H__ */
--- rtl819x/users/lsw-api/test.c
+++ rtl819x/users/lsw-api/test.c
@@ -6,13 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "lsw_api.h"
+
 
 int main(int argc, char **argv)
 {
 
- printf("how are you");
- 
- 
+ printf("how are you..\n");
+
+ rtl8198_set_vlan(8, 0x12f, 0, 0);
+ rtl8198_set_vlan(9, 0x12e, 0, 0);  
+ rtl8198_set_vlan(7, 0x12f, 0x01, 0);  
+
+ printf("done\n");
 
  return 0;
 }
