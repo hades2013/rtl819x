d80b96ee95ee0050ce3279d45d3111849ac2e10c
luoruncai <luoruncai@hexicomtech.com>
2015-01-06 10:15:26 +0800

    mod : fix a building error

---
+++ rtl819x/empty_folder.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+mkdir -p ./users/radvd-0.9.1/.deps
+mkdir -p ./users/ntfs-3g-2010.10.2/ntfs-3g-2010.10.2/m4
+mkdir -p ./users/samba-3.0.24/source/bin
+mkdir -p ./users/samba-3.0.24/patches
+mkdir -p ./users/libnl-1.1/lib/@echo
+mkdir -p ./users/libnl-1.1/doc/html
+mkdir -p ./users/busybox-1.13/shell/hush_test/hush-bugs
+mkdir -p ./users/dlna_dms/tmp
+mkdir -p ./users/openssl-0.9.8b/apps/demoCA/certs
+mkdir -p ./users/openssl-0.9.8b/apps/demoCA/newcerts
+mkdir -p ./users/openssl-0.9.8b/apps/demoCA_bak2/private
+mkdir -p ./toolchain/rsdk-1.3.6-4181-EB-2.6.30-0.9.30/config/uclibc/libpthread/linuxthreads/sysdeps/unix/sysv/linux/bits
+mkdir -p ./toolchain/rsdk-1.3.6-5281-EB-2.6.30-0.9.30/config/uclibc/libpthread/linuxthreads/sysdeps/unix/sysv/linux/bits
+mkdir -p ./boards/rtl8198/tmpfs/lib
+mkdir -p ./boards/rtl8198/home
+mkdir -p ./boards/rtl8198/romfs
+mkdir -p ./boards/rtl8198/image
+mkdir -p ./boards/rtl8196c/home
+
+exit 0
