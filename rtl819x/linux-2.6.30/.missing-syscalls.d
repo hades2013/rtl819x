4815fa663e9411be6fd7f0fd72738411ea421f6c
luoruncai <luoruncai@hexicomtech.com>
2015-11-06 15:35:22 +0800

    add building files

---
+++ rtl819x/linux-2.6.30/.missing-syscalls.d
@@ -0,0 +1,5 @@
+-:  ./include/linux/autoconf.h \
+  /opt/workspace/rtl8198/rtl819x/linux-2.6.30/arch/rlx/include/asm/types.h \
+  include/asm-generic/int-ll64.h include/asm-generic/bitsperlong.h \
+  /opt/workspace/rtl8198/rtl819x/linux-2.6.30/arch/rlx/include/asm/unistd.h \
+  /opt/workspace/rtl8198/rtl819x/linux-2.6.30/arch/rlx/include/asm/sgidefs.h
---
+++ rtl819x/linux-2.6.30/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 163 */
+#define UTS_MACHINE "rlx"
+#define UTS_VERSION "#163 Fri Nov 6 15:12:56 CST 2015"
+#define LINUX_COMPILE_TIME "15:12:56"
+#define LINUX_COMPILE_BY "lrc"
+#define LINUX_COMPILE_HOST "lrc"
+#define LINUX_COMPILE_DOMAIN ""
+#define LINUX_COMPILER "gcc version 3.4.6-1.3.6"
